d78e363dcdc8fc048545abbd07222b0e10530a45
MUH. AFRIZAL <87596110+afrizal0@users.noreply.github.com>
2023-01-03 07:10:57 +0800

    :)

--- app.py
+++ app.py
@@ -27,39 +27,47 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    fingerCount = 0
+    leftCount = 0
+    rightCount = 0
+
     if results.multi_hand_landmarks:
      for hand_landmarks in results.multi_hand_landmarks:
         # Get hand index to check label (left or right)
         handIndex = results.multi_hand_landmarks.index(hand_landmarks)
         handLabel = results.multi_handedness[handIndex].classification[0].label
-
+		
+        print(handIndex)
         # Set variable to keep landmarks positions (x and y)
         handLandmarks = []
 
         # Fill list with x and y positions of each landmark
         for landmarks in hand_landmarks.landmark:
           handLandmarks.append([landmarks.x, landmarks.y])
-
-        # Test conditions for each finger: Count is increased if finger is 
-        #   considered raised.
-        # Thumb: TIP x position must be greater or lower than IP x position, 
-        #   deppeding on hand label.
-        if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
-          fingerCount = fingerCount+1
-        elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
-          fingerCount = fingerCount+1
+        
+        print(handLandmarks[4][0])	
 
         # Other fingers: TIP y position must be lower than PIP y position, 
         #   as image origin is in the upper left corner.
-        if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
-          fingerCount = fingerCount+1
-        if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
-          fingerCount = fingerCount+1
-        if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
-          fingerCount = fingerCount+1
-        if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
-          fingerCount = fingerCount+1
+        if handLabel == "Left":
+          if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
+            leftCount = leftCount+1
+          if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
+            leftCount = leftCount+1
+          if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
+            leftCount = leftCount + 1
+          if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
+            leftCount = leftCount + 1
+
+        if handLabel == "Right":
+          if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
+            rightCount = rightCount+1
+          if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
+            rightCount = rightCount+1
+          if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
+            rightCount = rightCount + 1
+          if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
+            rightCount = rightCount + 1
+
 
         # Draw hand landmarks 
         mp_drawing.draw_landmarks(
@@ -70,7 +78,7 @@ with mp_hands.Hands(
             mp_drawing_styles.get_default_hand_connections_style())
 
     # Display finger count
-    cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
+    cv2.putText(image, str(leftCount + rightCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
     
     # Display image
     cv2.imshow('MediaPipe Hands', image)
